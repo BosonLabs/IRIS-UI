2b95f6d229ff5053a6137739e3fd1f5705041343
ramBoson <ramamugesh95@gmail.com>
2022-01-09 18:35:36 +0530

    https changed

--- src/configurl.js
+++ src/configurl.js
@@ -1,2 +1,3 @@
-const url = `http://3.144.242.33:42101/irisapi/v1`
+//const url = `https://3.144.242.33:42103/irisapi/v1`
+const url = `https://3.144.242.33:42103/elementsapi/v1/`;
 export default url;
